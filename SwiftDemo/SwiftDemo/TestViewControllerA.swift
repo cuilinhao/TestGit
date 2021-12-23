6f3ba45fe3bf9f75bde0e621f4168dc1382c0b60
崔林豪 <15021538370@163.com>
2021-12-23 22:49:21 +0800

    添加tableview

--- SwiftDemo/SwiftDemo/TestViewControllerA.swift
+++ SwiftDemo/SwiftDemo/TestViewControllerA.swift
@@ -32,54 +32,136 @@ class SomeClass: SomeSuperclass{
 
 class TestViewControllerA: UIViewController {
 
+	
+	lazy var tableView: UITableView = {
+		
+		let tableView = UITableView()
+		
+		tableView.dataSource = self
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+		 
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		
+		return tableView
+		
+	}()
+	
+	//MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemPurple
         let ss = SomeClass()
         ss.test()
-        
-        print("______ff")
-        
-//        self.beginAppearanceTransition(<#T##isAppearing: Bool##Bool#>, animated: <#T##Bool#>)
+		view.addSubview(tableView)
+		
+		NSLayoutConstraint.activate([
+		
+			tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+		])
+		
+		tableView.backgroundColor = .systemPurple
+		
+		testURLCompones1()
     }
-    
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        print(">>>>>viewDidAppear")
-        
-        
-        let num1 = NSDecimalNumber(string: "123.456")
-        let numaa = Decimal(123.456)
-        
-        let numbb = Decimal(string: "123.45")
-        
-        print("---\(num1)____\(numaa)___\(numbb)")
-        let num: NSNumber = 123.456
-        
-        let num2 = NSDecimalNumber(decimal: num.decimalValue)
-        let numberFormatter = NumberFormatter()
-        
-        numberFormatter.positiveFormat = "##,###.##"
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.roundingMode = .floor
-        
-        let amountStr1 = numberFormatter.string(from: num1) ?? "--"
-        print(amountStr1)
-        let amountStr2 = numberFormatter.string(from: num2) ?? "--"
-        print(amountStr2)
-        
-        // fontName:"Avenir-Black" size:18
-        
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        print(">>>>>viewWillDisappear")
-    }
+}
+
+extension TestViewControllerA: UITableViewDataSource, UITableViewDelegate {
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 20 + 20
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		
+		cell.textLabel?.text = indexPath.row.description
+		
+		return cell
+	}
+	
+	
+	
+ func tableView(_ tableView: UITableView,
+				   heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 45
+	}
+		
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+	}
+}
+
+extension TestViewControllerA {
+	
+	func testURLCompones1() {
+		
+		let searchTerm = "obi wan kenobi"
+		let format = "wookiee"
+
+		var urlComponents = URLComponents()
+		urlComponents.scheme = "https"
+		urlComponents.host = "swapi.co"
+		urlComponents.path = "/api/people"
+		urlComponents.queryItems = [
+		   URLQueryItem(name: "search", value: searchTerm),
+		   URLQueryItem(name: "format", value: format)
+		   ]
+		
+		
+		print(urlComponents.url?.absoluteString)
+
+		
+	}
+
+}
+
+
+
+extension TestViewControllerA {
+	
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		print(">>>>>viewDidAppear")
+		
+		//self.beginAppearanceTransition(T##isAppearing: Bool##Bool, animated: T##Bool)
+
+		let num1 = NSDecimalNumber(string: "123.456")
+		let numaa = Decimal(123.456)
+		
+		let numbb = Decimal(string: "123.45")
+		
+		print("---\(num1)____\(numaa)___\(numbb)")
+		let num: NSNumber = 123.456
+		
+		let num2 = NSDecimalNumber(decimal: num.decimalValue)
+		let numberFormatter = NumberFormatter()
+		
+		numberFormatter.positiveFormat = "##,###.##"
+		numberFormatter.numberStyle = .decimal
+		numberFormatter.roundingMode = .floor
+		
+		let amountStr1 = numberFormatter.string(from: num1) ?? "--"
+		print(amountStr1)
+		let amountStr2 = numberFormatter.string(from: num2) ?? "--"
+		print(amountStr2)
+		
+		// fontName:"Avenir-Black" size:18
+		
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		print(">>>>>viewWillDisappear")
+	}
+	
 }
 
 
