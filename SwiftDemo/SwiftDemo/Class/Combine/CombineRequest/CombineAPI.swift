1633057b7642786fb2ba041a80762df9adcd9cc4
Linhao CUI 崔林豪 <linhao.cui@nio.com>
2022-01-04 11:44:52 +0800

    ad test

--- SwiftDemo/SwiftDemo/Class/Combine/CombineRequest/CombineAPI.swift
+++ SwiftDemo/SwiftDemo/Class/Combine/CombineRequest/CombineAPI.swift
@@ -6,7 +6,26 @@
 //
 
 import UIKit
+import SwiftUI
+import Combine
 
-class CombineAPI: NSObject {
+//MARK: - 抽象基础网络能力, 方便扩展 代码如下:
 
+struct CombineAPI: View {
+
+    var body: some View {
+        //ReplistView(vm: .init())
+        EmptyView()
+    }
 }
+
+struct ReplistView: View {
+    
+   // @ObservedObject var vm: ReplistVM
+    
+    var body: some View {
+        EmptyView()
+        
+    }
+}
+
